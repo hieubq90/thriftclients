93c60cd41af52cf10377be181baa8469f2034d2f
hieubq <hieubq90@gmail.com>
2019-05-03 13:07:44 +0700

    add thrift client for PassportInfo & SimpleSession

---
+++ transports/passportinfo_client.go
@@ -0,0 +1,29 @@
+package transports
+
+import (
+	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/hieubq90/thriftclients/core/auth/Passport"
+	"github.com/hieubq90/thriftpool"
+)
+
+var (
+	passportBinaryMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
+		thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return (Passport.NewTPassportServiceClient(c)) }),
+		thriftpool.DefaultClose)
+
+	passportCommpactMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
+		thriftpool.GetThriftClientCreatorFuncCompactProtocol(func(c thrift.TClient) interface{} { return (Passport.NewTPassportServiceClient(c)) }),
+		thriftpool.DefaultClose)
+)
+
+//GetPassportBinaryClient client by host:port
+func GetPassportBinaryClient(aHost, aPort string) *thriftpool.ThriftSocketClient {
+	client, _ := passportBinaryMapPool.Get(aHost, aPort).Get()
+	return client
+}
+
+//GetPassportCompactClient get compact client by host:port
+func GetPassportCompactClient(aHost, aPort string) *thriftpool.ThriftSocketClient {
+	client, _ := passportCommpactMapPool.Get(aHost, aPort).Get()
+	return client
+}
---
+++ transports/simplesession_client.go
@@ -0,0 +1,29 @@
+package transports
+
+import (
+	"git.apache.org/thrift.git/lib/go/thrift"
+	ss "github.com/hieubq90/thriftclients/core/auth/SimpleSession"
+	thriftpool "github.com/hieubq90/thriftpool"
+)
+
+var (
+	ssMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
+		thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return (ss.NewTSimpleSessionService_WClient(c)) }),
+		thriftpool.DefaultClose)
+
+	ssMapPoolCompact = thriftpool.NewMapPool(1000, 3600, 3600,
+		thriftpool.GetThriftClientCreatorFuncCompactProtocol(func(c thrift.TClient) interface{} { return (ss.NewTSimpleSessionService_WClient(c)) }),
+		thriftpool.DefaultClose)
+)
+
+//GetSimpleSessionBinaryClient Get binary client by host:port
+func GetSimpleSessionBinaryClient(bsHost, bsPort string) *thriftpool.ThriftSocketClient {
+	client, _ := ssMapPool.Get(bsHost, bsPort).Get()
+	return client
+}
+
+//GetSimpleSessionCompactClient Get compact client by host:port
+func GetSimpleSessionCompactClient(bsHost, bsPort string) *thriftpool.ThriftSocketClient {
+	client, _ := ssMapPoolCompact.Get(bsHost, bsPort).Get()
+	return client
+}
