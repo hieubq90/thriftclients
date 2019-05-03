e7624092726a86ada8ed15e333a7a6c7356f9b50
hieubq <hieubq90@gmail.com>
2019-05-03 12:16:29 +0700

    add thrift client for BigSetGeneric & BigSetListI64

--- thrift/thriftgen.sh
+++ thrift/thriftgen.sh
@@ -2,3 +2,7 @@ thrift -r --gen go -out ../ kvstepcounter.thrift
 rm -Rf ../counter/KVStepCounter/*-remote
 thrift -r --gen go -out ../ s2i64kv.thrift
 rm -Rf ../common/S2I64KV/*-remote
+thrift -r --gen go -out ../ bigsetlistint.thrift
+rm -Rf ../bigset/ListI64/*-remote
+thrift -r --gen go -out ../ bigsetgenericdata.thrift
+rm -Rf ../bigset/Generic/*-remote
